bd9b44fbffff531369dd79ec2220473a5daf1af0
Ken Lee <wewert@gmail.com>
2018-04-29 23:33:46 -0600

    Initial Commit

---
+++ src/components/messages/InlineError.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const InlineError = ({ text }) => <span style={{ color: "ae5856" }}>{text}</span>;
+
+InlineError.propTypes = {
+  text: propTypes.string.isRequired
+};
+
+export default InlineError;
