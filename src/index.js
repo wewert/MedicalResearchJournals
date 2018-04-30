eb437466555d1030edfa16714a7a3a0dc236bc14
Ken Lee <wewert@gmail.com>
2018-04-30 15:34:14 -0600

    working login/logout

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+if (localStorage.medicalresearchjournalsJWT) {
+  const user = { token: localStorage.medicalresearchjournalsJWT };
+  store.dispatch(userLoggedIn(user));
+}
 
 ReactDOM.render(
   <BrowserRouter>
